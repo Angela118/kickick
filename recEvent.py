9bc09911b150588507fbabc2608e133430cbbccd
Angela <dlswn94@naver.com>
2018-08-24 20:17:01 +0900

    Add files via upload

--- recEvent.py
+++ recEvent.py
@@ -5,16 +5,19 @@ from scipy.spatial import distance
 from numpy import array
 from pymongo import MongoClient
 import warnings
+import sys
 
 warnings.filterwarnings('always')
 warnings.filterwarnings('ignore')
 
 
+
+
 def swap(x, i, j):
     x[i], x[j] = x[j], x[i]
- 
 
 
+'''
 conn = MongoClient('mongodb://localhost:27017')
 db = conn.local
 userCollection = db.users6
@@ -29,94 +32,90 @@ elif predict[1] == '남':
     predict[1] = 50
 elif predict[1] == '혼성':
     predict[1] = 25
-
-'''
-if predict[0] == 'O':
-    predict[0] = 100
-elif predict[0] == 'X':
-    predict[0] = 50
 '''
 
 
-print(predict)
+#파일 불러오기 
+file = open('C:/Users/user/brackets_nodejs/server/recEvent.csv', 'r')
+reader = csv.reader(file)
+
+data=[]
+for row_list in reader:
+    data.append(row_list)
+  
+file.close()
+
+print(data)
 print()
 
-done = 5
+del data[0]
 
-while(done):
-    #파일 불러오기 
-    file = open('C:/Users/user/brackets_nodejs/server/recEvent.csv', 'r')
-    reader = csv.reader(file)
+for i in range(0, len(data)):
+    if data[i][3] == '여자':
+        data[i][3] = 100
+    elif data[i][3] == '남자':
+        data[i][3] = 50
+    elif data[i][3] == '혼성':
+        data[i][3] = 25
 
-    data=[]
-    for row_list in reader:
-        data.append(row_list)
-  
-    file.close()
-
-    print(data)
-    print()
-
-    del data[0]
-
-    for i in range(0, len(data)):
-        if data[i][3] == '여자':
-            data[i][3] = 100
-        elif data[i][3] == '남자':
-            data[i][3] = 50
-        elif data[i][3] == '혼성':
-            data[i][3] = 25
         
-    dat = data[:]
+dat = data[:]
     
     
-    #data의 id값 
-    idArray = []
-    for i in range(0, len(data)):
-        idArray.append(data[i][0])
-        del data[i][0]
-
-    
-    for i in range(0, len(predict)):
-        predict[i] = float(predict[i])
+#data의 id값 
+idArray = []
+del data[0][0]
+for i in range(1, len(data)):
+    idArray.append(data[i][0])
+    del data[i][0]
 
 
-    predict = np.array(predict, dtype=np.float64)
-    data = np.array(data, dtype=np.float64)
+'''    
+for i in range(0, len(predict)):
+    predict[i] = float(predict[i])
+'''
+
+print(data)
+print()
+
+#predict = np.array(predict, dtype=np.float64)
+data = np.array(data, dtype=np.float64)
 
 
-    #euclidean distance
-    euc_dst = []
-    for i in range(0, len(data)):
-        euc_dst.append(distance.euclidean(predict, data[i]))
+#euclidean distance
+euc_dst = []
+for i in range(1, len(data)):
+    euc_dst.append(distance.euclidean(data[0], data[i]))
 
-    ar_euc = []
-    for i in range(0, len(euc_dst)):
-        ar_euc.append([i, euc_dst[i]])
-        
+print(euc_dst)
+print()
+ar_euc = []
+for i in range(0, len(euc_dst)):
+    ar_euc.append([i+1, euc_dst[i]])
+print(ar_euc)
+print()
 
-    #sorting    
-    for size in reversed(range(len(ar_euc))):
-        for i in range(size):
-            if ar_euc[i][1]>ar_euc[i+1][1]:
-                swap(ar_euc, i, i+1)
+#sorting    
+for size in reversed(range(len(ar_euc))):
+    for i in range(size):
+        if ar_euc[i][1]>ar_euc[i+1][1]:
+            swap(ar_euc, i, i+1)
                 
 
-    result_data = []
-    for i in range(0, len(ar_euc)):
-        result_data.append(dat[ar_euc[i][0]])
-        result_data[i].insert(0, idArray[i])
+result_data = []
+for i in range(0, len(ar_euc)):
+    result_data.append(dat[ar_euc[i][0]])
+    result_data[i].insert(0, idArray[i])
         
-    print(result_data)
+print(result_data)
 
 
-    #recOutput.csv에 저장 
-    with open('C:/Users/user/brackets_nodejs/server/recOutput.csv','w', newline='') as output:
-        writer = csv.writer(output)
-        for val in result_data:
-            writer.writerow(val)
-            
-    done=0
+#recOutput.csv에 저장 
+with open('C:/Users/user/brackets_nodejs/server/recOutput.csv','w', newline='') as output:
+    writer = csv.writer(output)
+    for val in result_data:
+        writer.writerow(val)
+
           
 print()
 
